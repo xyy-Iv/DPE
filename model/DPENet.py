ec11953289b5e787bfa5992de9ea3ff19243296b
xyy-Iv <xueyuyang@imperial-vision.com>
2018-11-06 16:58:24 +0800

    first commit

---
+++ model/DPENet.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn as nn
+from parts import *
+
+class DPENet(nn.Module):
+    def __init__(self, n_channels, n_classes):
+        super(DPENet, self).__init__()
+        self.inc = inclass(n_channels, 16)
+        self.down1 = down(16, 32)
+        self.down2 = down(32, 64)
+        self.down3 = down(64, 128)
+        self.down4 = down(128, 128)
+        self.pool = pool(128, 128)
+        self.fc = fc_expand(128, 128)
+        self.mconv1 = mid_conv(128, 128)
+        self.mconv2 = mid_conv(256, 128)
+        self.resi = resize(128, 128)
+        self.up1 = up(256, 128)
+        self.up2 = up(192, 64)
+        self.up3 = up(96, 32)
+        self.out1 = out_conv(48, 16)
+        self.out2 = out_conv(16, 3)
+        self.outc = outclass(3, n_classes)
+
+    def forward(self, x):
+        x_clone = x.clone()
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x6 = self.pool(x5)
+        x7 = self.pool(x6)
+        x8 = self.fc(x7)
+        x9 = self.mconv1(x5)
+        x10 = concat(x9, x8)
+        x11 = self.mconv2(x10)
+        x12 = self.resi(x11)
+        x = self.up1(x12, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.out1(x)
+        x = self.out2(x)
+        x = self.outc(x, x_clone)
+
+net = DPENet(3, 3)
+print(net)
---
+++ model/parts.py
@@ -0,0 +1,129 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class selu(nn.Module):
+    def __init__(self):
+        super(selu, self).__init__()
+
+    def forward(self, x):
+        alpha = 1.6732632423543772848170429916717
+        scale = 1.0507009873554804934193349852946
+        
+        return scale * F.elu(x, alpha)
+
+class inclass(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(inclass, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 5, stride=2),
+            selu(),
+            nn.BatchNorm2d(out_ch)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+class down(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(down, self).__init__()
+        self.mpconv = nn.Sequential(
+            inclass(in_ch, out_ch),
+            nn.MaxPool2d(2)
+        )
+
+    def forward(self, x):
+        x = self.mpconv(x)
+        return x
+
+class pool(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(pool, self).__init__()
+        self.poolconv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 5, stride=2),
+            nn.MaxPool2d(2)
+        )
+    
+    def forward(self, x):
+        x = self.poolconv(x)
+        return x
+
+class fc_expand(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(fc_expand, self).__init__()
+        self.fceconv = nn.Sequential(
+            nn.Linear(in_ch, out_ch, bias=False),
+            selu()
+        )
+    
+    def forward(self, x, ex):
+        x = self.fceconv(x)
+        x = x.expand([ex, ex, x.shape(2)])
+        print(x.shape)
+        return x
+
+class mid_conv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(mid_conv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_ch, out_ch, 5, stride=2)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+class concat(nn.Module):
+    def __init__(self):
+        super(concat, self).__init__()
+        
+    def forward(self, x1, x2):
+        x = torch.cat([x1, x2], dim=1)
+        return x
+
+class resize(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(resize, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode='bilinear')
+      )
+    
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+class out_conv(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(out_conv, self).__init__()
+        self.conv = nn.Sequential(
+            selu(),
+            nn.BatchNorm2d(in_ch),
+            nn.Conv2d(in_ch, out_ch, 5, stride=2)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+class up(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(up, self).__init__()
+        self.conv = out_conv(in_ch, out_ch)
+    def forward(self, x1, x2):
+        x1 = resize.conv(x1)
+        x = torch.cat([x1, x2], dim=1)
+        x = self.conv(x)
+        return x
+
+
+class outclass(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(outclass, self).__init__()
+        self.conv = out_conv(in_ch, out_ch)
+    
+    def forward(self, x1, x2):
+        x = self.conv(x1)
+        x = torch.add(x, x2)
+        return x
