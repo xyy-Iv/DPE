33a493c80856e8783b3e0bc8626c27f10486805a
xyy-Iv <xueyuyang@imperial-vision.com>
2018-11-07 21:25:09 +0800

    add test file

---
+++ test.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+import os
+
+import torch
+import torchvision.transforms as transforms
+
+from dataset import *
+
+dataset = 'Danbooru'
+model = 'checkpoint/Danbooru/netG_model_epoch_200.pth'
+ngpu = 1
+
+netG = torch.load(model)
+
+image_dir = 'dataset/' + dataset + '/test/a/'
+image_filenames = [x for x in os.listdir(image_dir) if is_image_file(x)]
+
+transform_list = [transforms.ToTensor(),
+                  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+
+transform = transforms.Compose(transform_list)
+
+for image_name in image_filenames:
+    img = load_img(image_dir + image_name)
+    img = transform(img)
+    input = img.view(1, -1, 512, 512)
+
+    if ngpu:
+        netG = netG.cuda()
+        input = input.cuda()
+
+    out = netG(input)
+    out = out.cpu()
+    out_img = out.data[0]
+    if not os.path.exists(os.path.join("result", dataset)):
+        os.makedirs(os.path.join("result", dataset))
+    save_img(out_img, "result/{}/{}".format(dataset, image_name))
